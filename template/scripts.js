1cfda158d515ae43ffae9e60be99f3ad7ad53c95
Taha. Dostifam <mr.tahadostifam@gmail.com>
2022-01-01 19:52:08 +0330

    Signin & Signup pages design finished

---
+++ template/scripts.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify({
+        theme: {
+            dark: true
+        }
+    }),
+    data(){
+        return {
+            theme_color: "green accent-4"
+        }
+    }
+})
