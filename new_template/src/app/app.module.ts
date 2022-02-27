122475371699f6e989b5b2e0eab1fd2845c96522
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-02-27 23:38:55 +0330

    .

--- new_template/src/app/app.module.ts
+++ new_template/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { ChatComponent } from './views/chat/chat.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SigninComponent, ChatComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
