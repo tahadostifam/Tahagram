4409abf6f31aa2a0e02d3be64347757cb6133030
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-03-01 20:40:43 +0330

    .

--- new_template/src/app/views/chat/chat.component.ts
+++ new_template/src/app/views/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -6,9 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.less'],
 })
 export class ChatComponent implements OnInit {
+  @ViewChild('showMenuButton') menu_button!: ElementRef;
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  search_mode: boolean = true;
+  search_mode: boolean = false;
+
+  rotateIcon(el: ElementRef) {
+    if (!el.nativeElement.classList.contains('rotate_icon')) {
+      el.nativeElement.classList.add('rotate_icon');
+      setTimeout(() => {
+        el.nativeElement.classList.remove('rotate_icon');
+      }, 300);
+    }
+  }
+
+  menuButtonClick() {
+    if (this.search_mode == true) {
+      this.search_mode = false;
+      this.rotateIcon(this.menu_button);
+      return;
+    }
+    this.rotateIcon(this.menu_button);
+  }
+
+  searchInputFocus() {
+    this.search_mode = true;
+    this.rotateIcon(this.menu_button);
+  }
 }
