8bc37dbba33deec609f595c5e7c0497e46976491
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-03-02 00:42:45 +0330

    .

--- new_template/src/app/views/chat/chat.component.ts
+++ new_template/src/app/views/chat/chat.component.ts
@@ -7,12 +7,14 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
   @ViewChild('showMenuButton') menu_button!: ElementRef;
+  @ViewChild('searchChatsList') search_chats_list!: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   search_mode: boolean = false;
+  show_search_chats_list: boolean = false;
   search_input: string = '';
 
   rotateIcon(el: ElementRef) {
@@ -24,9 +26,24 @@ export class ChatComponent implements OnInit {
     }
   }
 
+  setSearchChatsListState(state: boolean) {
+    if (!state) {
+      this.search_chats_list.nativeElement.classList.remove('show');
+      setTimeout(() => {
+        this.show_search_chats_list = false;
+      }, 100);
+    } else {
+      this.show_search_chats_list = true;
+      setTimeout(() => {
+        this.search_chats_list.nativeElement.classList.add('show');
+      }, 10);
+    }
+  }
+
   menuButtonClick() {
     if (this.search_mode == true) {
       this.search_mode = false;
+      this.setSearchChatsListState(false);
       this.rotateIcon(this.menu_button);
       this.search_input = '';
       return;
@@ -35,6 +52,7 @@ export class ChatComponent implements OnInit {
 
   searchInputFocus() {
     this.search_mode = true;
+    this.setSearchChatsListState(true);
     this.rotateIcon(this.menu_button);
   }
 
