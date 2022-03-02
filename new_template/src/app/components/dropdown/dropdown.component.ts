b846c59ca219cfa15e68d1f4c537c2298991ade2
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-03-02 21:37:35 +0330

    .

--- new_template/src/app/components/dropdown/dropdown.component.ts
+++ new_template/src/app/components/dropdown/dropdown.component.ts
@@ -7,22 +7,27 @@ import { Component, OnInit, Input, ElementRef } from '@angular/core';
 export class DropdownComponent implements OnInit {
   @Input('id') id!: string;
   @Input('width') width!: string;
-  @Input('offset') offset!: string;
+  @Input('offset_top') offset_top?: string;
+  @Input('offset_bottom') offset_bottom?: string;
   @Input('rtl') rtl!: boolean;
+  @Input('bottom') bottom?: boolean;
   @Input('large_items') large_items!: boolean;
 
   constructor(private elref: ElementRef) {}
 
   ngOnInit(): void {}
 
+  remove_anim: any = null;
+
   closeDropDown(_id: string) {
     const el: HTMLElement = this.elref.nativeElement.querySelector(
       `#${_id} .dropdown .dropdown-content`
     );
+    clearTimeout(this.remove_anim);
     el.classList.remove('show');
-    setTimeout(() => {
+    this.remove_anim = setTimeout(() => {
       el.style.display = 'none';
-    }, 200);
+    }, 150);
   }
 
   showDropDown(_id: string) {
@@ -32,6 +37,6 @@ export class DropdownComponent implements OnInit {
     el.style.display = 'block';
     setTimeout(() => {
       el.classList.add('show');
-    }, 2);
+    }, 1);
   }
 }
