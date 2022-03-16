6a9cb7c8547a5330e7152b7edc126a5494c01f8b
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-03-16 22:15:03 +0330

    Angular based template removed

--- new_template/src/app/directives/lazy-image-loading.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LazyImageLoadingDirective } from './lazy-image-loading.directive';
-
-describe('LazyImageLoadingDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LazyImageLoadingDirective();
-    expect(directive).toBeTruthy();
-  });
-});
