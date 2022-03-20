ca85b7b867c4f215eb073cc2512e4d72e39cae0d
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-03-20 17:53:24 +0330

    angular based frontend moved here

--- frontend/pages/index.vue
+++
@@ -1,6 +0,0 @@
-<script>
-export default {
-  name: "index",
-  middleware: ["need_auth"],
-};
-</script>
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { ChatComponent } from './views/chat/chat.component';
+import { ChatRowComponent } from './components/chat-row/chat-row.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { MessageComponent } from './components/message/message.component';
+import { SettingsPanelComponent } from './components/settings-panel/settings-panel.component';
+import { LazyImageLoadingDirective } from './directives/lazy-image-loading.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    ChatComponent,
+    ChatRowComponent,
+    DropdownComponent,
+    MessageComponent,
+    SettingsPanelComponent,
+    LazyImageLoadingDirective,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
