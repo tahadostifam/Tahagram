c5adc9f4532bd2e35a11ed1665b7198982c17f44
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-03-24 20:33:21 +0430

    .

--- backend/mail/mail.ts
+++ backend/mail/mail.ts
@@ -4,34 +4,6 @@ import ejs from "ejs";
 const mail_configs = require("../configs/mail.json");
 
 const templates_directory = `${process.cwd()}/mail`;
-let templates: any = {};
-
-async function readAndSetTemplates() {
-    return new Promise<void>((resolve, reject) => {
-        const templates_list = [{ filename: "verific", data: {} }];
-        templates_list.forEach(async (item, index) => {
-            const file_addr = `${templates_directory}/${item.filename}.ejs`;
-            await fs.readFile(file_addr, async (err, data) => {
-                if (err) {
-                    reject();
-                    return console.error(`An error occurred on reading ${item.filename} mail template`);
-                }
-                const rendered_template = await ejs.render(data.toString("utf-8"), item.data, { async: true });
-                console.log(rendered_template);
-                templates["hello"] = "world";
-
-                // templates[item.filename] = rendered_template;
-            });
-            if (index == templates_list.length - 1) {
-                resolve();
-            }
-        });
-    });
-}
-
-readAndSetTemplates().then(() => {
-    console.log(templates);
-});
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -41,26 +13,30 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-export function sendMail(template_name: string, to: string, subject: string) {
+export function sendMail(template_name: string, data: any, to: string, subject: string) {
     return new Promise<void>((resolve, reject) => {
-        const html = fs.exists(`${process.cwd()}/mail/${template_name}`, (files_exists) => {
-            if (files_exists) {
-                const mail = transporter
-                    .sendMail({
-                        from: "Tahagram's Service",
-                        to: to,
-                        subject: subject,
-                        html: "",
-                    })
-                    .then(() => {
-                        resolve();
-                    })
-                    .catch(() => {
-                        reject();
-                    });
-            } else {
+        ejs.renderFile(`${templates_directory}/${template_name}.ejs`, data, (err, str) => {
+            if (err) {
                 reject();
+                return console.error(`An error occurred on reading ${template_name} mail template`);
             }
+            const mail = transporter
+                .sendMail({
+                    from: "Tahagram's Service",
+                    to: to,
+                    subject: subject,
+                    html: str,
+                })
+                .then(() => {
+                    resolve();
+                })
+                .catch(() => {
+                    reject();
+                });
         });
     });
 }
+
+sendMail("verific", {}, "mr.tahadostifam@gmail.com", "Test!").then(() => {
+    console.log("email sended");
+});
