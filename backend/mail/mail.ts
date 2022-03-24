3c3da78c949fed2a2033d5a73d808eca8977ec8d
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-03-24 19:58:04 +0430

    working on mail_service template reader

--- backend/mail/mail.ts
+++ backend/mail/mail.ts
@@ -1,6 +1,37 @@
 import nodemailer from "nodemailer";
 import fs from "fs";
+import ejs from "ejs";
 const mail_configs = require("../configs/mail.json");
+
+const templates_directory = `${process.cwd()}/mail`;
+let templates: any = {};
+
+async function readAndSetTemplates() {
+    return new Promise<void>((resolve, reject) => {
+        const templates_list = [{ filename: "verific", data: {} }];
+        templates_list.forEach(async (item, index) => {
+            const file_addr = `${templates_directory}/${item.filename}.ejs`;
+            await fs.readFile(file_addr, async (err, data) => {
+                if (err) {
+                    reject();
+                    return console.error(`An error occurred on reading ${item.filename} mail template`);
+                }
+                const rendered_template = await ejs.render(data.toString("utf-8"), item.data, { async: true });
+                console.log(rendered_template);
+                templates["hello"] = "world";
+
+                // templates[item.filename] = rendered_template;
+            });
+            if (index == templates_list.length - 1) {
+                resolve();
+            }
+        });
+    });
+}
+
+readAndSetTemplates().then(() => {
+    console.log(templates);
+});
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
