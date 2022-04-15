ae9e30dfd6b23d20b2f0cb6209434901f4a7cd40
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-04-15 21:56:59 +0430

    bug fix

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -12,12 +12,12 @@ import jsCookie from "js-cookie"
 export default Vue.extend({
   name: "IndexPage",
   mounted(){
-    const authToken = jsCookie.get("auth_token")
-    if (authToken && authToken.trim().length > 0) {
-      this.$router.push({ path: '/' + (this as any).$i18n.locale + '/chat' });
-    } else {
-      this.$router.push({ path: '/' + (this as any).$i18n.locale + '/signin' });
-    }
+    // const authToken = jsCookie.get("auth_token")
+    // if (authToken && authToken.trim().length > 0) {
+    //   this.$router.push({ path: '/' + (this as any).$i18n.locale + '/chat' });
+    // } else {
+    //   this.$router.push({ path: '/' + (this as any).$i18n.locale + '/signin' });
+    // }
   }
 })
 </script>
