8818eae48beade7ac4b6bd04bb74007585986de4
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-04-15 21:16:44 +0430

    an unknown error with store :/

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -11,8 +11,13 @@ import jsCookie from "js-cookie"
 
 export default Vue.extend({
   name: "IndexPage",
-  created(){
-    jsCookie
+  mounted(){
+    const authToken = jsCookie.get("auth_token")
+    if (authToken && authToken.trim().length > 0) {
+      this.$router.push({ path: '/' + (this as any).$i18n.locale + '/chat' });
+    } else {
+      this.$router.push({ path: '/' + (this as any).$i18n.locale + '/signin' });
+    }
   }
 })
 </script>
